a98e969a2fd5eb007621490a860e6bb035138c9c
Johannes Hertenstein <johannes@nimius.net>
2018-08-26 18:43:11 +0200

    FEATURE | Spawn multiple workers for parallel requests

--- cmd/requester/main.go
+++ cmd/requester/main.go
@@ -6,11 +6,16 @@ import (
 	"flag"
 )
 
+var (
+	workers = flag.Int("workers", 8, "The number of workers to use")
+)
+
+
 func main() {
 	flag.Parse()
 	scanner := Scanner{}
 
-	log.Printf("%#v", flag.Args())
+	// Initialize parser with command line arguments
 	for _, base := range(flag.Args()) {
 		if strings.Index(base, "http") == -1 {
 			base = "http://" + base
@@ -18,13 +23,24 @@ func main() {
 		scanner.Initialize(base)
 	}
 
-	log.Printf("%#v", scanner)
-	for {
-		if !scanner.Work() {
-			break
-		}
+	// Setup thread pool
+	var wg sync.WaitGroup
+	for w := 1; w <= *workers; w++ {
+		wg.Add(1)
+		go func() {
+			for {
+				if !scanner.Work() {
+					break
+				}
+			}
+			wg.Done()
+		}()
 	}
 
+	// Wait for All workers to terminate
+	wg.Wait()
+
+	// Print stats
 	for status, requests := range(scanner.RequestsByStatus()) {
 		log.Printf("## %v: %d Requests", status, len(requests))
 		if status == "200 OK" {
